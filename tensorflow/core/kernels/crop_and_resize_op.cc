36fbdcb1235348ecdac045c6041ece6542b26ee5
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2018-08-16 18:15:03 +0300

    Fix CropAndResize op

--- tensorflow/core/kernels/crop_and_resize_op.cc
+++ tensorflow/core/kernels/crop_and_resize_op.cc
@@ -240,7 +240,8 @@ struct CropAndResize<CPUDevice, T> {
           const float in_y = (crop_height > 1)
                                  ? y1 * (image_height - 1) + y * height_scale
                                  : 0.5 * (y1 + y2) * (image_height - 1);
-          if (in_y < 0 || in_y > image_height - 1) {
+          const int top_y_index = floorf(in_y);
+          if (top_y_index < 0 || top_y_index > image_height - 1) {
             for (int x = 0; x < crop_width; ++x) {
               for (int d = 0; d < depth; ++d) {
                 crops(b, y, x, d) = extrapolation_value;
@@ -249,7 +250,6 @@ struct CropAndResize<CPUDevice, T> {
             continue;
           }
           if (method_name == "bilinear") {
-            const int top_y_index = floorf(in_y);
             const int bottom_y_index = ceilf(in_y);
             const float y_lerp = in_y - top_y_index;
 
@@ -257,13 +257,13 @@ struct CropAndResize<CPUDevice, T> {
               const float in_x = (crop_width > 1)
                                      ? x1 * (image_width - 1) + x * width_scale
                                      : 0.5 * (x1 + x2) * (image_width - 1);
-              if (in_x < 0 || in_x > image_width - 1) {
+              const int left_x_index = floorf(in_x);
+              if (left_x_index < 0 || left_x_index > image_width - 1) {
                 for (int d = 0; d < depth; ++d) {
                   crops(b, y, x, d) = extrapolation_value;
                 }
                 continue;
               }
-              const int left_x_index = floorf(in_x);
               const int right_x_index = ceilf(in_x);
               const float x_lerp = in_x - left_x_index;
 
@@ -287,7 +287,8 @@ struct CropAndResize<CPUDevice, T> {
               const float in_x = (crop_width > 1)
                                      ? x1 * (image_width - 1) + x * width_scale
                                      : 0.5 * (x1 + x2) * (image_width - 1);
-              if (in_x < 0 || in_x > image_width - 1) {
+              const int left_x_index = floorf(in_x);
+              if (left_x_index < 0 || left_x_index > image_width - 1) {
                 for (int d = 0; d < depth; ++d) {
                   crops(b, y, x, d) = extrapolation_value;
                 }
@@ -464,10 +465,10 @@ struct CropAndResizeBackpropImage<CPUDevice, T> {
         const float in_y = (crop_height > 1)
                                ? y1 * (image_height - 1) + y * height_scale
                                : 0.5 * (y1 + y2) * (image_height - 1);
-        if (in_y < 0 || in_y > image_height - 1) {
+        const int top_y_index = floorf(in_y);
+        if (top_y_index < 0 || top_y_index > image_height - 1) {
           continue;
         }
-        const int top_y_index = floorf(in_y);
         const int bottom_y_index = ceilf(in_y);
         const float y_lerp = in_y - top_y_index;
 
@@ -475,11 +476,11 @@ struct CropAndResizeBackpropImage<CPUDevice, T> {
           const float in_x = (crop_width > 1)
                                  ? x1 * (image_width - 1) + x * width_scale
                                  : 0.5 * (x1 + x2) * (image_width - 1);
-          if (in_x < 0 || in_x > image_width - 1) {
+          const int left_x_index = floorf(in_x);
+          if (left_x_index < 0 || left_x_index > image_width - 1) {
             continue;
           }
           if (method_name == "bilinear") {
-            const int left_x_index = floorf(in_x);
             const int right_x_index = ceilf(in_x);
             const float x_lerp = in_x - left_x_index;
 
@@ -645,10 +646,10 @@ struct CropAndResizeBackpropBoxes<CPUDevice, T> {
         const float in_y = (crop_height > 1)
                                ? y1 * (image_height - 1) + y * height_scale
                                : 0.5 * (y1 + y2) * (image_height - 1);
-        if (in_y < 0 || in_y > image_height - 1) {
+        const int top_y_index = floorf(in_y);
+        if (top_y_index < 0 || top_y_index > image_height - 1) {
           continue;
         }
-        const int top_y_index = floorf(in_y);
         const int bottom_y_index = ceilf(in_y);
         const float y_lerp = in_y - top_y_index;
 
@@ -656,10 +657,10 @@ struct CropAndResizeBackpropBoxes<CPUDevice, T> {
           const float in_x = (crop_width > 1)
                                  ? x1 * (image_width - 1) + x * width_scale
                                  : 0.5 * (x1 + x2) * (image_width - 1);
-          if (in_x < 0 || in_x > image_width - 1) {
+          const int left_x_index = floorf(in_x);
+          if (left_x_index < 0 || left_x_index > image_width - 1) {
             continue;
           }
-          const int left_x_index = floorf(in_x);
           const int right_x_index = ceilf(in_x);
           const float x_lerp = in_x - left_x_index;
 
